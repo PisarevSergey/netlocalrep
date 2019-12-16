9545d1a6c19b98c13203ecbeb80f05e4b11ad048
Sergey Pisarev <pisarev.sergey@gmail.com>
2019-12-16 22:06:56 +0300

    tracing.h

--- netlocalrep/main.cpp
+++ netlocalrep/main.cpp
@@ -1,6 +1,13 @@
 #include "nlr_common.h"
+#include "main.tmh"
 
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT /*driver*/, PUNICODE_STRING)
 {
+  WPP_INIT_TRACING(0, 0);
+
+  info_message(MAIN, "test");
+
+  WPP_CLEANUP(0);
+
   return STATUS_UNSUCCESSFUL;
 }
--- netlocalrep/nlr_common.h
+++ netlocalrep/nlr_common.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #include <fltKernel.h>
+
+#include "tracing.h"
+
